a16df32d8e43cf11c7ad7950183da6fd060d47a2
Diego Mego <DM2593@gmail.com>
2018-12-07 20:11:19 -0500

    Intro to GET and POST

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 #Set the root path
 app = Flask(__name__)
@@ -7,13 +7,24 @@ app = Flask(__name__)
 def index():
     return 'This is the Homepage'
 
-@app.route('/tuna')
-def tuna():
-    return '<h2>Tuna is Good</h2>'
+@app.route('/bacon', methods=['GET', 'POST'])
+def bacon():
+    if request.method == 'POST':
+        return 'You are using POST'
+    else:
+        return 'You are probably using GET'
 
-@app.route('/profile/<username>')
-def profile(username):
-    return '<h1>Hello {}!'.format(username)
+# @app.route('/tuna')
+# def tuna():
+#     return '<h2>Tuna is Good</h2>'
+#
+# @app.route('/profile/<username>')
+# def profile(username):
+#     return '<h1>Hello {}!'.format(username)
+#
+# @app.route('/post/<int:post_id>')
+# def post(post_id):
+#     return '<h1>Hello {}!'.format(post_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
