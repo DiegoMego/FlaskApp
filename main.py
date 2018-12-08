a20cc0c95b2ecf738b960a8f146db8722972f494
Diego Mego <DM2593@gmail.com>
2018-12-07 19:37:08 -0500

    Working with routes

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return 'This is the Homepage'
 
+@app.route('/tuna')
+def tuna():
+    return '<h2>Tuna is Good</h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return '<h1>Hello {}!'.format(username)
+
 if __name__ == '__main__':
     app.run(debug=True)
