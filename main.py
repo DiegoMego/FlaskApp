19cef9830d64bccc2d9c2de1352418b6cf6d3c84
Diego Mego <DM2593@gmail.com>
2018-12-07 21:09:46 -0500

    Working with Static Files - Intro

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 #Set the root path
 app = Flask(__name__)
@@ -7,12 +7,16 @@ app = Flask(__name__)
 def index():
     return 'This is the Homepage'
 
-@app.route('/bacon', methods=['GET', 'POST'])
-def bacon():
-    if request.method == 'POST':
-        return 'You are using POST'
-    else:
-        return 'You are probably using GET'
+@app.route('/profile/<name>')
+def profile(name):
+    #Look for a file 'profile.html' in the directory templates (this is the default directory for flask).
+    return render_template('profile.html', name=name)
+# @app.route('/bacon', methods=['GET', 'POST'])
+# def bacon():
+#     if request.method == 'POST':
+#         return 'You are using POST'
+#     else:
+#         return 'You are probably using GET'
 
 # @app.route('/tuna')
 # def tuna():
